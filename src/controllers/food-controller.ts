8e3c83be0e8713b6c88b415d94b7e488b4701b54
lucasvlrn <lucasvilarinhop@gmail.com>
2025-04-24 23:14:56 -0300

    :mag: feat: filtros disponíveis para busca de dados do banco

--- src/controllers/food-controller.ts
+++ src/controllers/food-controller.ts
@@ -2,49 +2,52 @@
 import { Request, Response } from "express";
 const foodsModel = require("../models/food-model");
 
-
 const fetchFoods = async (request: Request, response: Response) => {
-    const { name, type } = request.query
+  const { name, type } = request.query;
 
-    if (name && !type) {
-        try {
-            const foods = await foodsModel.fetchFoodsWithName(name);
-            return response.status(200).json({ success: `Aqui está o(a) ${name}`, foods })
-        }
-        catch (err) {
-            return response.status(500).json({ Error: "Erro do servidor", err })
-        }
-    }
-    if (!name && type) {
-        try {
-            const foods = await foodsModel.fetchFoodsWithType(type);
-            return response.status(200).json({ success: `Aqui estão os alimentos}`, foods })
-        }
-        catch (err) {
-            return response.status(500).json({ Error: "Erro do servidor", err })
-        }
-    }
-
+  if (name && !type) {
     try {
-        const foods = await foodsModel.fetchFoods();
-        return response.status(200).json({ success: "Todas as comidas listadas", foods })
+      const foods = await foodsModel.fetchFoodsByName(name);
+      return response
+        .status(200)
+        .json({ success: `Aqui está o(a) ${name}`, foods });
+    } catch (err) {
+      return response.status(500).json({ Error: "Erro do servidor", err });
     }
-    catch (err) {
-        return response.status(500).json({ Error: "Erro do servidor ", err })
+  }
+  if ((!name && type == "fruta") || type == "legume" || type == "vegetal") {
+    try {
+      const foods = await foodsModel.fetchFoodsByType(type);
+      return response
+        .status(200)
+        .json({ success: `Alimentos filtrados por '${type}'`, foods });
+    } catch (err) {
+      return response.status(500).json({ Error: "Erro do servidor", err });
     }
-}
+  }
+
+  try {
+    const foods = await foodsModel.fetchFoods();
+    return response
+      .status(200)
+      .json({ success: "Todas as comidas listadas", foods });
+  } catch (err) {
+    return response.status(500).json({ Error: "Erro do servidor ", err });
+  }
+};
 
 const insertFoods = async (request: Request, response: Response) => {
-    try {
-        const foods = await foodsModel.insertFoods();
-        return response.status(201).json({ success: "Inserido todos os alimentos", foods })
-    }
-    catch (err) {
-        return response.status(500).json({ Error: "Erro do servidor", err })
-    }
-}
+  try {
+    const foods = await foodsModel.insertFoods();
+    return response
+      .status(201)
+      .json({ success: "Inserido todos os alimentos", foods });
+  } catch (err) {
+    return response.status(500).json({ Error: "Erro do servidor", err });
+  }
+};
 
 module.exports = {
-    fetchFoods,
-    insertFoods
-}
+  fetchFoods,
+  insertFoods,
+};
--- src/models/food-model.ts
+++ src/models/food-model.ts
@@ -1,54 +1,61 @@
-import { connectToDatabase } from "../config/db-config";
-const foodjson = require('../food.json')
+import { connectToDatabase, disconnectToDatabase } from "../config/db-config";
+const foodjson = require("../food.json");
 
 async function fetchFoods() {
-    try {
-        const db = await connectToDatabase();
-        const findFoods = await db.collection("foods").find().toArray();
-        console.log(findFoods);
-        return findFoods;
-    } catch (err) {
-        console.error("Não foi encontrado");
-    }
+  try {
+    const db = await connectToDatabase();
+    const findFoods = await db.collection("foods").find().toArray();
+    console.log(findFoods);
+    await disconnectToDatabase();
+    return findFoods;
+  } catch (err) {
+    console.error("Não foi encontrado");
+  }
 }
 
-async function fetchFoodsWithName(name: string) {
-    try {
-        const db = await connectToDatabase()
-        const findFood = await db.collection("foods").findOne({ name: name });
-        console.log(findFood);
-        return findFood
-    } catch (err) {
-        console.error("Não encontrado")
-    }
+async function fetchFoodsByName(name: string) {
+  try {
+    const db = await connectToDatabase();
+    const findFood = await db
+      .collection("foods")
+      .findOne({ name: { $regex: name, $options: "i" } });
+    console.log(findFood);
+    await disconnectToDatabase();
+    return findFood;
+  } catch (err) {
+    console.error("Não encontrado", err);
+  }
 }
 
-async function fetchFoodsWithType(type: string) {
-    try {
-        const db = await connectToDatabase()
-        const findFoods = await db.collection("foods").find({ type: type }).toArray();
-        console.log(findFoods);
-        return findFoods;
-    } catch (err) {
-        console.error(err)
-    }
+async function fetchFoodsByType(type: string) {
+  try {
+    const db = await connectToDatabase();
+    const findFoods = await db
+      .collection("foods")
+      .find({ type: { $regex: type, $options: "i" } })
+      .toArray();
+    console.log(findFoods);
+    await disconnectToDatabase();
+    return findFoods;
+  } catch (err) {
+    console.error("Não encontrado", err);
+  }
 }
-
 
 async function insertFoods() {
-    try {
-        const db = await connectToDatabase();
-        const insertFoods = await db.collection("foods").insertMany(foodjson)
-        console.log(insertFoods);
-    }
-    catch (err) {
-        console.error("Não foi encontrado");
-    }
+  try {
+    const db = await connectToDatabase();
+    const insertFoods = await db.collection("foods").insertMany(foodjson);
+    console.log(insertFoods);
+    await disconnectToDatabase();
+  } catch (err) {
+    console.error("Não foi encontrado");
+  }
 }
 
 module.exports = {
-    fetchFoods,
-    insertFoods,
-    fetchFoodsWithName,
-    fetchFoodsWithType
-}
+  fetchFoods,
+  insertFoods,
+  fetchFoodsByName,
+  fetchFoodsByType,
+};
