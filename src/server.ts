43ae65eae1f21cd580aa56ee5d07b16076bef409
lucasvlrn <lucasvilarinhop@gmail.com>
2025-04-24 23:15:38 -0300

    :package: adição de novas dependências ao projeto

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,6 @@ const app = require("./config/utils");
 require("dotenv").config();
 
 const port = process.env.PORT || 3333;
-
 app.listen(port, () => {
-    console.log(`Server running on port ${port} 🚀`);
+  console.log(`Server running on port ${port} 🚀`);
 });
